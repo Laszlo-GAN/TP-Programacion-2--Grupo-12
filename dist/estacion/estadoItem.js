e35c56d17d30cb6c21474622d8e338d40b6edde8
Laszlo Ganduglia <laszloganduglia@gmail.com>
2026-09-06 16:07:11 -0300

    Definir bien las clases

---
+++ dist/estacion/estadoItem.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.EstadoItem = void 0;
+class EstadoItem {
+    estado() {
+        return "";
+    }
+}
+exports.EstadoItem = EstadoItem;
+//# sourceMappingURL=estadoItem.js.map
---
+++ dist/pedidos/estadoPedido.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.EstadoPedido = void 0;
+class EstadoPedido {
+    estado;
+    constructor() {
+        this.estado = "";
+    }
+}
+exports.EstadoPedido = EstadoPedido;
+//# sourceMappingURL=estadoPedido.js.map
