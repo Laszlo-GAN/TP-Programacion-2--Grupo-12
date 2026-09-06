10fc1d90cc01486bafe609585e34b687c2d82a48
Manuel Aranda <manuivan2004@gmail.com>
2026-09-06 16:47:50 -0300

    Escrita clase Producto

--- src/estacion/producto.ts
+++ src/estacion/producto.ts
@@ -1,3 +1,14 @@
-export class Producto {
-        
+export abstract class Producto {
+    private nombre: string;
+    private categoria: string;
+    abstract precioBase: number;
+
+    constructor(){
+        this.nombre = "";
+        this.categoria = "";
+    }
+
+    public abstract precioConBeneficio(): number;
+    public abstract estacionDeCocina(): string;
+    public abstract beneficio(): number;
 }
