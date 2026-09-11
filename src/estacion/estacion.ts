8990ba0d566f242ecfa3a0cf266109452fb28297
Luem-fern <luem.fern@gmail.com>
2026-09-10 22:28:31 -0300

    Add files via upload

--- src/estacion/estacion.ts
+++ src/estacion/estacion.ts
@@ -1,7 +1,15 @@
+import { Pedido } from "../pedidos/pedido";
+
+
 export abstract class Estacion {
     private nombre: string;
-    private colaItems: string;
+    protected colaItems: Pedido[];
 
+    constructor(){
+        this.nombre = "";
+        this.colaItems = [];
+    }
     public abstract recibirItems(): void;
+    
     public abstract procesarSiguiente(): void;
 }
