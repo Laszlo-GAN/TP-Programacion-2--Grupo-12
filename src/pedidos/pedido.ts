61395c3c2fcf08becca04e33bb9d88ca0b0c6ec2
Luem-fern <luem.fern@gmail.com>
2026-09-15 18:18:45 -0300

    Add files via upload

--- src/pedidos/pedido.ts
+++ src/pedidos/pedido.ts
@@ -1,18 +1,35 @@
-import { EstadoPedido } from "./estadoPedido";
+import { EstadoItem } from "../estacion/estadoItem";
+import { EstadoEstacion } from "../estacion/estadoEstacion";
+import { TipoEstacion } from "./TipoEstacion";
+
 export abstract class Pedido {
     private id: string;
     private fechaHora: Date;
-    private estado: EstadoPedido;
+    private estadoItem: EstadoItem;
+    private tipoEstacion = TipoEstacion;
+
+    constructor(id: string, fechaHora: Date, estado:EstadoEstacion, tipoEstaciion: TipoEstacion){
+        this.id = id;
+        this.fechaHora = fechaHora;
+        this.estadoItem = EstadoItem.PENDIENTE;
+        this.tipoEstacion = TipoEstacion;
+    }
+    public getEstadoItem(): EstadoItem{
+        return this.estadoItem;
+    }
+    public enPreparacion(): void{
+        this.estadoItem = EstadoItem.EN_PREPARACION;
+    }
+    public enListo(): void{
+        this.estadoItem = EstadoItem.LISTO;
+    }
+    public getTipoEstacion(): TipoEstacion{
+        return this.tipoEstacion;
+    }
     public abstract DatosDelPedido(): string;
-    public agregarProducto(): void {
-
-    }
-    public quitarProducto(): void {
-
-    }
-    public deshacerUltimaModificacion(): void {
-
-    }
+    public agregarProducto(): void {}
+    public quitarProducto(): void {}
+    public deshacerUltimaModificacion(): void {    }
     public puedeFacturarse(): boolean {
         return true;
     }
@@ -20,3 +37,4 @@ export abstract class Pedido {
         return 0;
     }
 }
+
