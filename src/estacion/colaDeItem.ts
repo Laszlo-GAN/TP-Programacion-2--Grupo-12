b83b7cedbf9cdd4807ab6cb046c604bafd9dac6c
Manuel Aranda <manuivan2004@gmail.com>
2026-09-19 02:26:52 -0300

    Vuelta a comentar clase ColaItems

--- src/estacion/colaDeItem.ts
+++ src/estacion/colaDeItem.ts
@@ -1,25 +1,27 @@
-import { Pedido } from "../pedidos/pedido";
-import { Estacion } from "./estacion";
-import { Producto } from "./producto";
-
-class ColaItems extends Estacion {
-
-    public colaItems: Pedido[];
-    
-    constructor(colaItems: ColaItems){
-        super();
-        this.colaItems = [];
-    }
-    
-    public recibirItems(): void{
-     //   this.colaItems.push(nuevoPedido);
-    }
-    
-    public procesarSiguiente(): void{
-
-    }
-
-    protected aQueEstacionVa(): void {
-        
-    }
-}
+/** Esto no va por ahora
+ * 
+ * import { Pedido } from "../pedidos/pedido";
+ * import { Estacion } from "./estacion";
+ * import { Producto } from "./producto";
+ * 
+ * class ColaItems extends Estacion {
+ * 
+ *  public colaItems: Pedido[];
+ * 
+ *  constructor(colaItems: ColaItems){
+ *      super();
+ *      this.colaItems = [];
+ *  }
+ *  public recibirItems(): void{
+ *      this.colaItems.push(nuevoPedido);
+ *  }
+ * 
+ *  public procesarSiguiente(): void{
+ * 
+ *  }
+ * 
+ *  protected aQueEstacionVa(): void {
+ *     
+ *  }
+ * }
+ */
