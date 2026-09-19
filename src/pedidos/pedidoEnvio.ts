bd58d1f01aea498e6a6789efe3980e0dcc2046d4
Manuel Aranda <manuivan2004@gmail.com>
2026-09-19 02:28:58 -0300

    Agregado super() de la clase PedidoEnvio

--- src/pedidos/pedidoEnvio.ts
+++ src/pedidos/pedidoEnvio.ts
@@ -1,11 +1,13 @@
+import { EstadoEstacion } from "../estacion/estadoEstacion";
 import { Pedido } from "./pedido";
+import { TipoEstacion } from "./TipoEstacion";
 
 export class PedidoEnvio extends Pedido {
     private direccion: string;
     private costoEnvio: number;
 
-    constructor(){
-        super();
+    constructor(id: string, fechaHora: Date, estado: EstadoEstacion, tipoEstacion: TipoEstacion){
+        super(id, fechaHora, estado, tipoEstacion);
         this.direccion = "";
         this.costoEnvio = 0;
     }
